b83e28292bdf699a20d108b11f0105e194ad7ec8
ZhiQiangHou <hpuhouzhiqiang@gmail.com>
2014-02-02 01:22:27 +0800

    first commit

---
+++ RenameMusic/RenameMusic/FactoryForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace RenameMusic
+{
+    partial class FactoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelAddFile = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.txtBoxFile = new System.Windows.Forms.TextBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.labTime = new System.Windows.Forms.Label();
+            this.listFileName = new System.Windows.Forms.ListBox();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAddFile
+            // 
+            this.labelAddFile.AutoSize = true;
+            this.labelAddFile.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelAddFile.Location = new System.Drawing.Point(12, 32);
+            this.labelAddFile.Name = "labelAddFile";
+            this.labelAddFile.Size = new System.Drawing.Size(79, 20);
+            this.labelAddFile.TabIndex = 1;
+            this.labelAddFile.Text = "添加文件夹";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 351);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始改名";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Location = new System.Drawing.Point(132, 351);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(75, 23);
+            this.btnCancle.TabIndex = 3;
+            this.btnCancle.Text = "取消";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // txtBoxFile
+            // 
+            this.txtBoxFile.Location = new System.Drawing.Point(162, 31);
+            this.txtBoxFile.Name = "txtBoxFile";
+            this.txtBoxFile.Size = new System.Drawing.Size(168, 21);
+            this.txtBoxFile.TabIndex = 6;
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(347, 29);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 8;
+            this.btnAddFile.Text = "添加";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.Location = new System.Drawing.Point(14, 287);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(0, 12);
+            this.labTime.TabIndex = 9;
+            this.labTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listFileName
+            // 
+            this.listFileName.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listFileName.FormattingEnabled = true;
+            this.listFileName.ItemHeight = 17;
+            this.listFileName.Location = new System.Drawing.Point(16, 124);
+            this.listFileName.Name = "listFileName";
+            this.listFileName.ScrollAlwaysVisible = true;
+            this.listFileName.Size = new System.Drawing.Size(406, 140);
+            this.listFileName.TabIndex = 13;
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblFileCount.Location = new System.Drawing.Point(13, 82);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(0, 17);
+            this.lblFileCount.TabIndex = 14;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(240, 351);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 444);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "作者：强子";
+            // 
+            // FactoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 468);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.listFileName);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.txtBoxFile);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.labelAddFile);
+            this.Name = "FactoryForm";
+            this.Text = "FaactoryForm";
+            this.Load += new System.EventHandler(this.FactoryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelAddFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.TextBox txtBoxFile;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.ListBox listFileName;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ RenameMusic/RenameMusic/FactoryForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RenameMusic
+{
+    public partial class FactoryForm : Form
+    {
+        public FactoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+
+            //使用FolderBrowserDialog类，它提示用户浏览、创建并最终选择一个文件夹
+            FolderBrowserDialog fole = new FolderBrowserDialog();
+
+            //设置是否显示“新建文件夹”按钮  (false为不显示  true为显示)
+            fole.ShowNewFolderButton = true;
+            //设置显示提示标题文字
+            fole.Description = "请选择媒体文件夹";
+            //判断是否点击了确定按钮
+            if (fole.ShowDialog() == DialogResult.OK)
+            {
+                //实例化目录信息的类 (文件夹的全路径)
+                DirectoryInfo dir = new DirectoryInfo(fole.SelectedPath);
+
+                txtBoxFile.Text = fole.SelectedPath.ToString();
+
+
+                //获取总文件数目MP3和wma
+                int nFileCount = 0;
+                FileInfo[] file = dir.GetFiles();
+
+
+                for (int i = 0; i < file.Length ; i++)
+                {
+                    if (file[i].Extension.ToLower().Equals(".mp3") || file[i].Extension.ToLower().Equals(".wma"))
+                    {
+                        nFileCount = nFileCount + 1;
+                    }
+                }
+
+                lblFileCount.Text = "总歌曲数是：" + nFileCount.ToString() + "首";
+
+                //遍历文件夹中所有文件(除子文件夹)
+                foreach (FileInfo info in dir.GetFiles())
+                {
+                    //判断文件的后缀名 进行筛选
+                    if (info.Extension.ToLower().Equals(".mp3") || info.Extension.ToLower().Equals(".wma"))
+                    {
+                        //把筛选后的文件添加到ListBox控件的集合中
+                        listFileName.Items.Add(info.FullName);
+                    }
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            DateTime strStartTime = DateTime.Now;
+
+            for (int i = 0; i < listFileName.Items.Count; i++)
+            {
+                string strOldName = listFileName.Items[i].ToString();
+                string strFileName = strOldName.Substring(strOldName.LastIndexOf('\\') + 1);
+
+                //获取歌手和歌曲名和文件格式
+                string strSinger = strFileName.Substring(0,strFileName.IndexOf('-')).Trim();
+                string strMusicName = strFileName.Substring(strFileName.IndexOf('-') + 1, (strFileName.LastIndexOf('.') - strFileName.IndexOf('-'))-1).Trim();
+                string strFormat = strFileName.Substring(strFileName.LastIndexOf('.'));
+                string strNewName=strOldName.Substring(0,strOldName.LastIndexOf('\\')+1)+strMusicName+'-'+strSinger+strFormat;
+                //更改歌曲名称
+                File.Move(strOldName, strNewName);
+            }
+
+
+
+            DateTime strEndTime = DateTime.Now;
+            TimeSpan span = strEndTime - strStartTime;
+            this.labTime.Text = "花费时间为：" + (span.TotalMilliseconds).ToString();
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            txtBoxFile.Text = "";
+            listFileName.Items.Clear();
+        }
+
+        private void FactoryForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("您确定要退出？");
+            this.Close();
+            
+        }
+    }
+}
